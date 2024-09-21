b1f4c56f49ae1cd960e4f900ab0ff5be689c569d
John Doty <john@d0ty.me>
2024-09-21 07:45:59 -0700

    [wadler] One more bit of writing.

--- parser/wadler/__init__.py
+++ parser/wadler/__init__.py
@@ -1,4 +1,12 @@
 # A prettier printer.
+"""Generate [Wadler-style](https://homepages.inf.ed.ac.uk/wadler/papers/prettier/prettier.pdf)
+pretty printers from grammars.
+
+Use the functions in the [builder] module to generate tables from grammars.
+
+You can then feed those tables into a generic pretty-printer implementation,
+like what we have in the [runtime] module.
+"""
 from . import builder
 from . import runtime
 
